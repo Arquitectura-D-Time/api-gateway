80b2ae756d37d16841d397ce6d8945ddc4498370
Rafael Cruz <ercruzr@unal.edu.co>
2019-10-18 20:29:01 -0500

    edicion docker-compose y creacion carpeta authentication con archivos authresolvers, authserver y authtypeDef

---
+++ src/authentication/authresolvers.js
@@ -0,0 +1,4 @@
+import { generalRequest, getRequest } from '../utilities';
+import { url, port, entryPoint } from './authserver';
+
+const URL = `http://${url}:${port}/${entryPoint}`;
---
+++ src/authentication/authserver.js
@@ -0,0 +1,3 @@
+export const url = process.env.AUTH_URL || 'authentication_ms'
+export const port =  process.env.AUTH_PORT || '5001'
+export const entryPoint = process.env.AUTH_ENTRY || 'auth';
