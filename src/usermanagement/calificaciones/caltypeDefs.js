4c5dc3678c1cc3ed189d7d6c4b6cce40bf4b7091
Denis <dearodriguezve@unal.edu.co>
2019-10-30 11:50:58 -0500

    correccion parametro

--- src/usermanagement/calificaciones/caltypeDefs.js
+++ src/usermanagement/calificaciones/caltypeDefs.js
@@ -20,7 +20,7 @@ input CalificacionInput2 {
 export const calificacionesQueries = `
     getCalificaciones: [Calificacion]!
     calificacionById(idcalifico: Int!, idcalificado: Int!): Calificacion!
-    getCalificacionesByID: [Calificacion]!
+    getCalificacionesByID(idcalificado: Int!): [Calificacion]!
 `;
 
 export const calificacionesMutations = `
