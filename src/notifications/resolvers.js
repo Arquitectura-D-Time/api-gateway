75698602eb71f5228419bce84ceac49b21f5d419
David Fernando <dafguerreroal@unal.edu.co>
2019-11-26 17:24:07 -0500

    modified search requests

--- src/notifications/resolvers.js
+++ src/notifications/resolvers.js
@@ -7,15 +7,15 @@ const resolvers = {
 	Query: {
 		test: (_) =>
 			getRequest(`${URL}`,'all'),
-		getById: (_, { id }) =>
-			generalRequest(`${URL}/${id}`, 'GET'),
-		getByToIdUser:(_, {toUserId}) =>
+		getById: (_, { _id }) =>
+			generalRequest(`${URL}/${_id}`, 'GET'),
+		getByToIdUser: (_, {toUserId}) =>
 			generalRequest(`${URL}/toUser/${toUserId}`, 'GET'),
-		getByFromIdUser:(_, {fromUserId}) =>
+		getByFromIdUser: (_, {fromUserId}) =>
 			generalRequest(`${URL}/toUser/${fromUserId}`, 'GET'),
-		getBytoUserStatus:(_, {toUserId, status}) =>
+		getBytoUserStatus: (_, {toUserId, status}) =>
 			generalRequest(`${URL}/toUserAndStatus/${toUserId, status}`, 'GET'),
-		getByfromUserStatus:(_, {fromUserId, status}) =>
+		getByfromUserStatus: (_, {fromUserId, status}) =>
 			generalRequest(`${URL}/fromUserAndStatus/${fromUserId, status}`, 'GET'),
 	},
 	Mutation: {
